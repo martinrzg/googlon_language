9ce89574a69203fe3cb9148fcb1fe76833a1f158
martin <martin.ruiz8811@gmail.com>
2019-03-11 23:31:32 -0600

    Clean code

--- translator/cmd/main.go
+++ translator/cmd/main.go
@@ -1,20 +1,9 @@
-package cmd
+package main
 
-import "fmt"
+import "os"
 
 func main() {
-
-	//TODO uncomment line
-	//argFilename := os.Args[1]
-	text := newScrollFromFile("resource/input.txt")
-	fmt.Println("Scroll word count: ", len(text.words))
-
-	prep, verb, verbSubj, number := text.classify()
-
-	orderVocabulary := distinctWords(prep, verb, verbSubj, number).sortVocabulary()
-
-	fmt.Println(len(prep.words), len(verb.words), len(verbSubj.words), len(number.words))
-
-	fmt.Println(orderVocabulary, len(orderVocabulary.words))
-
+	argFilename := os.Args[1]
+	text := newScrollFromFile(argFilename)
+	text.printScrollSummary()
 }
