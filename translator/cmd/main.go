a7f4ce441611bacd5c546380c47b0693ff3e1897
martin <martin.ruiz8811@gmail.com>
2019-03-12 01:03:34 -0600

    Add execution time for benchmark

--- translator/cmd/main.go
+++ translator/cmd/main.go
@@ -1,9 +1,15 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+	"time"
+)
 
 func main() {
+	start := time.Now()
 	argFilename := os.Args[1]
 	text := newScrollFromFile(argFilename)
 	text.printScrollSummary()
+	fmt.Printf("\n Glooglon report took %v \n", time.Since(start))
 }
